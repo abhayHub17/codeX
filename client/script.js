345193061ec0916e57138b17149489b247a6eced
abhayjeet sharma <abhay.abhayjeetsharma17@gmail.com>
2022-12-26 17:21:49 +0530

    update

--- client/script.js
+++ client/script.js
@@ -1,3 +1,125 @@
+// import bot from "./assets/bot.svg";
+// import user from "./assets/user.svg";
+
+// const form = document.querySelector("form");
+// const chatContainer = document.querySelector("#chat_container");
+
+// let loadInterval;
+
+// function loader(element) {
+//   element.textContent = "";
+
+//   loadInterval = setInterval(() => {
+//     // Update the text content of the loading indicator
+//     element.textContent += ".";
+
+//     // If the loading indicator has reached three dots, reset it
+//     if (element.textContent === "....") {
+//       element.textContent = "";
+//     }
+//   }, 300);
+// }
+
+// function typeText(element, text) {
+//   let index = 0;
+
+//   let interval = setInterval(() => {
+//     if (index < text.length) {
+//       element.innerHTML += text.charAt(index);
+//       index++;
+//     } else {
+//       clearInterval(interval);
+//     }
+//   }, 20);
+// }
+
+// //to genearte a unique id for every single msg so that we can map over them
+// function generateUniqueId() {
+//   const timestamp = Date.now();
+//   const randomNumber = Math.random();
+//   const hexadecimalString = randomNumber.toString(16);
+
+//   return `id-${timestamp}-${hexadecimalString}`;
+// }
+
+// //chat stripe function to know who wrote the msg The user or the AI
+// function chatStripe(isAi, value, uniqueId) {
+//   return `
+//     <div class="wrapper ${isAi && "ai"}">
+//       <div class="chat">
+//         <div class="profile">
+//           <img
+//             src="${isAi ? bot : user}"
+//             alt="${isAi ? bot : user}"
+//           />
+//         </div>
+//         <div class="message" id=${uniqueId}>${value}</div>
+//       </div>
+//     </div>
+//     `;
+// }
+
+// const handleSubmit = async (e) => {
+//   e.preventDefault();
+
+//   const data = new FormData(form);
+
+//   //user's chat stripe
+//   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+//   form.reset();
+
+//   //bot's chat stripe
+//   const uniqueId = generateUniqueId();
+//   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+//   //this is gonna put the new msg in view
+//   chatContainer.scrollTop = chatContainer.scrollHeight;
+
+//   //to fetch the newly created div
+//   const messageDiv = document.getElementById(uniqueId);
+
+//   loader(messageDiv);
+
+//   // fetch data from server -> bot's response
+//   // const response = await fetch("https://codex1-bbus.onrender.com/", {
+//   const response = await fetch("http://localhost:3000/", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       prompt: data.get("prompt"),
+//     }),
+//   });
+//   clearInterval(loadInterval);
+//   messageDiv.innerHTML = " ";
+
+//   if (response.ok) {
+//     const data = await response.json();
+//     const parsedData = data.bot.trim();
+
+//     typeText(messageDiv, parsedData);
+//   } else {
+//     const err = await response.text();
+
+//     messageDiv.innerHTML = "Something went wrong";
+
+//     alert(err);
+//   }
+// };
+
+// //to see the changes that we made to our handleSubmit
+// form.addEventListener("submit", handleSubmit);
+
+// //to submit via enter key also
+// form.addEventListener("keyup", (e) => {
+//   if (e.keyCode === 13) {
+//     //13 is enter key
+//     handleSubmit(e);
+//   }
+// });
+
 import bot from "./assets/bot.svg";
 import user from "./assets/user.svg";
 
@@ -33,7 +155,9 @@ function typeText(element, text) {
   }, 20);
 }
 
-//to genearte a unique id for every single msg so that we can map over them
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
 function generateUniqueId() {
   const timestamp = Date.now();
   const randomNumber = Math.random();
@@ -42,20 +166,19 @@ function generateUniqueId() {
   return `id-${timestamp}-${hexadecimalString}`;
 }
 
-//chat stripe function to know who wrote the msg The user or the AI
 function chatStripe(isAi, value, uniqueId) {
   return `
-    <div class="wrapper ${isAi && "ai"}">
-      <div class="chat">
-        <div class="profile">
-          <img
-            src="${isAi ? bot : user}"
-            alt="${isAi ? bot : user}"
-          />
+        <div class="wrapper ${isAi && "ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? "bot" : "user"}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
         </div>
-        <div class="message" id=${uniqueId}>${value}</div>
-      </div>
-    </div>
     `;
 }
 
@@ -64,25 +187,26 @@ const handleSubmit = async (e) => {
 
   const data = new FormData(form);
 
-  //user's chat stripe
+  // user's chatstripe
   chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
+  // to clear the textarea input
   form.reset();
 
-  //bot's chat stripe
+  // bot's chatstripe
   const uniqueId = generateUniqueId();
   chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
-  //this is gonna put the new msg in view
+  // to focus scroll to the bottom
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  //to fetch the newly created div
+  // specific message div
   const messageDiv = document.getElementById(uniqueId);
 
+  // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  // fetch data from server -> bot's response
-  const response = await fetch("https://codex1-bbus.onrender.com/", {
+  const response = await fetch("http://localhost:3000/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -91,30 +215,26 @@ const handleSubmit = async (e) => {
       prompt: data.get("prompt"),
     }),
   });
+
   clearInterval(loadInterval);
   messageDiv.innerHTML = " ";
 
   if (response.ok) {
     const data = await response.json();
-    const parsedData = data.bot.trim();
+    const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
 
     typeText(messageDiv, parsedData);
   } else {
     const err = await response.text();
 
     messageDiv.innerHTML = "Something went wrong";
-
     alert(err);
   }
 };
 
-//to see the changes that we made to our handleSubmit
 form.addEventListener("submit", handleSubmit);
-
-//to submit via enter key also
 form.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
-    //13 is enter key
     handleSubmit(e);
   }
 });
